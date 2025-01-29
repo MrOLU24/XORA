e491e734cd7b958a33f54833e8fb4c1749fef464
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-29 14:25:35 +0100

    adding overlay on menu

--- src/Sections/header.jsx
+++ src/Sections/header.jsx
@@ -4,7 +4,7 @@ import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
 const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5">
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5 max-lg:text-2xl">
     {title}
   </LinkScroll>
 );
@@ -16,7 +16,12 @@ const Header = () => {
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
-        <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', isMenuOpen ? 'max-lg:opacity-100' : 'max-lg:pointer-events-none')}>
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isMenuOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:p-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -26,7 +31,15 @@ const Header = () => {
                   <NavLink title="pricing" />
                 </li>
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -42,6 +55,22 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[290px] -translate-y-1/2 rotate-90">
+              <img
+                className="relative z-2"
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+              />
+              <img
+                className="ablolute inset-0 mix-blend-soft-light opacity-5"
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+              />
+            </div>
           </div>
         </div>
         <button
