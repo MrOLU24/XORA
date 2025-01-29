8468f5dee8a4e41676eaa84c43374a39739121ad
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-29 15:50:43 +0100

    creating a button coponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Header from "./Sections/header";
+import Hero from "./sections/Hero";
 
 function App() {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 }
