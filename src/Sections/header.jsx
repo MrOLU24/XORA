605fdc7de6093400080602fc6265aea96a16d349
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-29 13:26:10 +0100

    done with the toggle

--- src/Sections/header.jsx
+++ src/Sections/header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 const NavLink = ({ title }) => (
@@ -6,22 +7,15 @@ const NavLink = ({ title }) => (
     {title}
   </LinkScroll>
 );
-
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
-        {/* Mobile Navigation */}
-        <div
-          className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 transition-opacity duration-500 ${
-            isOpen ? "max-lg:opacity-100 max-lg:pointer-events-auto" : "max-lg:opacity-0 max-lg:pointer-events-none"
-          }`}
-        >
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:p-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -49,16 +43,17 @@ const Header = () => {
             </nav>
           </div>
         </div>
-        {/* Hamburger Button */}
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => setIsMenuOpen((preState) => !preState)}
         >
-          <img src="/images/magic.svg" alt="burger" />
+          <img
+            src={`/images/${isMenuOpen ? "close" : "magic"}.svg`}
+            alt="burger"
+          />
         </button>
       </div>
     </header>
   );
 };
-
 export default Header;
