39ca43dc3088f22cfa06ba8073f6db2479d70975
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-30 13:37:11 +0100

    features

---
+++ src/Contants/index.js
@@ -0,0 +1,25 @@
+export const features = [
+    {
+        id: '0',
+        icon: "/images/feature-1.png",
+        caption: "DEMO CAPTION",
+        title: "FEATURE 1",
+        text: "text for feature 1",
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'watch the DEMO',
+        }
+    },
+    {
+        id: '1',
+        icon: "/images/feature-2.png",
+        caption: "DEMO CAPTION",
+        title: "FEATURE 2",
+        text: "text for feature 2",
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'watch the DEMO',
+        }
+    }
+    
+];
