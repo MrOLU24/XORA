8b61e47adc060fbc95c839b50d317c118384b9ae
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-30 14:00:03 +0100

    added features icon

--- src/Sections/Features.jsx
+++ src/Sections/Features.jsx
@@ -14,7 +14,8 @@ const Features = () => {
                 className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
               >
                 <div className="w-full flex justify-start items-start">
-                  <div className="">
+                  <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
+                    <div className="w-0.5 h-16 bg-s3 "/>
                     <img
                       src={icon}
                       className="size-28 object-contain"
