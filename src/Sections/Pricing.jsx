a03e90b074d7f0db213c64ac178778293eeba939
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-01 23:58:40 +0100

    bugs

--- src/Sections/Pricing.jsx
+++ src/Sections/Pricing.jsx
@@ -1,15 +1,22 @@
+import clsx from "clsx";
 import React from "react";
 import { Element } from "react-scroll";
+import clsx from "clsx";
 
 const Pricing = () => {
+  const [monthly, setMonthly] = useState(false);
   return (
     <section>
       <Element name="pricing">
         <div className="container">
-          <div className="max-w-960 border-2 border-red-500">
-            <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative max-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
-              Flexible pricing for teams of all sizes of all
+          <div className="max-w-968 border-2 border-red-500">
+            <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
+              Flexible pricing for teams of all sizes
             </h3>
+            <div>
+              <button className={clsx('pricing-head_btn', monthly && 'text-p4' )} onClick={() => setMonthly(true)}>Monthly</button>
+              <button className={clsx('pricing-head_btn', !monthly && 'text-p4' )}  onClick={() => setMonthly(false)}>Annual</button>
+            </div>
           </div>
         </div>
       </Element>
