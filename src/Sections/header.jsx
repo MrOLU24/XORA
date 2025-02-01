3b61ba35c4fa21ec168df54456cea1fa3a9a36c1
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-01 18:48:43 +0100

    done with the navigation of hero and features

--- src/Sections/header.jsx
+++ src/Sections/header.jsx
@@ -3,11 +3,6 @@ import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5 max-lg:text-2xl">
-    {title}
-  </LinkScroll>
-);
 const Header = () => {
   const [hasScrolled, setHasScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,9 +17,27 @@ const Header = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsMenuOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5 max-lg:text-2xl"
+    >
+      {title}
+    </LinkScroll>
+  );
 
   return (
-    <header className={clsx("fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4", hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
