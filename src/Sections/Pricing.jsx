d76e5ccc1220350b5265e39c5516cf5540c0df65
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-03 23:59:32 +0100

    done

--- src/Sections/Pricing.jsx
+++ src/Sections/Pricing.jsx
@@ -55,12 +55,13 @@ const Pricing = () => {
           <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-lg:overflow-auto max-xl:pt-6">
             {plans.map((plan, index) => (
               <div
-                className="pricing-plan_first pricing-plan_last pricing-plan_add pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
                 key={plan.id}
+                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
               >
                 {index === 1 && (
                   <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
                 )}
+
                 <div
                   className={clsx(
                     "absolute left-0 right-0 z-2 flex items-center justify-center",
@@ -68,12 +69,12 @@ const Pricing = () => {
                   )}
                 >
                   <img
+                    src={plan.logo}
+                    alt={plan.title}
                     className={clsx(
                       "object-contain drop-shadow-2xl",
                       index === 1 ? "size-[120px]" : "size-[88px]"
                     )}
-                    src={plan.logo}
-                    alt={plan.title}
                   />
                 </div>
               </div>
