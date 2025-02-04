e00c2aabc81ff6b0856319b753a0a9d50aeec706
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-04 23:48:13 +0100

    faq head

--- src/Sections/Faq.jsx
+++ src/Sections/Faq.jsx
@@ -4,9 +4,16 @@ import { Element } from "react-scroll";
 const Faq = () => {
   return (
     <Element name="faq" className="relative">
-      <div>
-        <h1 className="h3 max">Curiousity didnt kill the Cat, it gave it answers.</h1>
-        <p>You've got questions, we've got answers.</p>
+      <div className="container relative z-2 py-28">
+        <div>
+          <h1 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
+            Curiousity didnt kill the Cat, it gave it answers.
+          </h1>
+          <p className="body-1 max-lg:max-w-sm">
+            You've got questions, we've got answers.
+          </p>
+        </div>
+        <div/>
       </div>
     </Element>
   );
