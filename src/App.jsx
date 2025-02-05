f05261e18092f7b44e5dfa77522f32d5f51bcc0b
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-05 15:53:35 +0100

    fully downgraded to react v17

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Sections/header";
+import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
@@ -7,18 +7,20 @@ import Faq from "./sections/Faq";
 import Testimonial from "./sections/Testimonial";
 import Download from "./sections/Download";
 import Footer from "./sections/Footer";
+
 function App() {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
-      <Features/>
-      <Pricing/>
-      <Faq/>
-      <Testimonial/>
-      <Download/>
-      <Footer/>
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonial />
+      <Download />
+      <Footer />
     </main>
   );
 }
+
 export default App;
