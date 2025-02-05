3061593dac084b934ddda8800e25e48e087574c7
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-05 17:18:34 +0100

    haeder import remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Hero from "./sections/Hero.jsx";
-import Header from "./sections/Header.jsx"
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
@@ -11,7 +10,6 @@ import Footer from "./sections/Footer.jsx";
 function App() {
   return (
     <main className="overflow-hidden">
-    <Header/>
       <Hero />
       <Features />
       <Pricing />
