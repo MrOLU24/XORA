3beb4afe4a908b05ea39c0d6f26f9597e3d4aabd
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-08 23:29:07 +0100

    snipper

---
+++ src/Components/Loadingspinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loadingspinner = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Loadingspinner;
